e59cd38deb4bb2eb7dfdd4007e203b00ad221899
nimmicv <nimmicv@gmail.com>
2014-09-25 20:14:21 -0700

    merge

--- LearningLabs/LearningLabs/urls.py
+++ LearningLabs/LearningLabs/urls.py
@@ -12,8 +12,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^', views.helloWorld),
-    url(r'Register', TemplateView.as_view(template_name='./register.html')), 
     url(r'registerUser$', views.registerUser),
     url(r'countpolls', views.getPolls),
     url(r'counter', TemplateView.as_view(template_name='./counter.html')),
