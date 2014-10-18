4066fe8fc809a9ecc1cf188d61289c65ee3946b5
palloabhi <pallavikhandekar212@gmail.com>
2014-10-18 15:54:45 -0700

    Removed Answers table and renamed AudienceAnswer to Answer

--- LearningLabs/learning_labs/models.py
+++ LearningLabs/learning_labs/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from djangotoolbox.fields import ListField
 import django_tables2 as tables
 
-# Create your models here.
+
+#Login / Registration data model
 class Register(models.Model):
     fname = models.TextField()
     lname = models.TextField()
@@ -10,35 +11,32 @@ class Register(models.Model):
     email= models.TextField()
     password = models.TextField()
 
+#Stores Quiz and associated questions and answers choices if any.
 class Quiz(models.Model):
     quizname = models.TextField()
     question = models.TextField()
     answerchoices = ListField()
     correctAnswers = ListField()
-    
+   
+#TODO: Remove / Merge with QUIZ 
 class QuestionsTable(tables.Table):
     Questions= tables.Column()
     Options=tables.Column()
     Answer = tables.Column()
     
-# class Answers(models.Model):
-#     quizquestionid = models.TextField()
-#     userid = models.TextField()
-#     answer = models.TextField()
-    
-class AudincePost(models.Model):
+class Answers(models.Model):
     studentId = models.TextField()
     questionId = models.TextField()
     answer = models.TextField()
 
-class Answers(models.Model):
-    Name = models.TextField()
-    ID = models.IntegerField()
-    SIS_ID = models.IntegerField()
-    Section = models.TextField()
-    Section_Id = models.IntegerField()
-    Section_SIS_Id= models.TextField()
-    Question_Id= models.IntegerField()
-    Question= models.TextField()
-    Submitted= models.TextField()
-    Answer= models.TextField()
+# class Answers(models.Model):
+#     Name = models.TextField()
+#     ID = models.IntegerField()
+#     SIS_ID = models.IntegerField()
+#     Section = models.TextField()
+#     Section_Id = models.IntegerField()
+#     Section_SIS_Id= models.TextField()
+#     Question_Id= models.IntegerField()
+#     Question= models.TextField()
+#     Submitted= models.TextField()
+#     Answer= models.TextField()
