5819515fb10f84138aeaaf68dcbc397bdd91bfe5
Pratiksha Pai <pratikshavpai@gmail.com>
2014-10-20 18:57:11 -0700

    poll timer and disbling sumssion after timeout

--- LearningLabs/learning_labs/models.py
+++ LearningLabs/learning_labs/models.py
@@ -3,25 +3,29 @@ from djangotoolbox.fields import ListField
 import django_tables2 as tables
 
 
-#Login / Registration data model
+# Login / Registration data model
 class Register(models.Model):
     fname = models.TextField()
     lname = models.TextField()
-    usrname=models.TextField()
-    email= models.TextField()
+    usrname = models.TextField()
+    email = models.TextField()
     password = models.TextField()
 
-#Stores Quiz and associated questions and answers choices if any.
+# Stores Quiz and associated questions and answers choices if any.
 class Quiz(models.Model):
-    quizname = models.TextField()
-    question = models.TextField()
-    answerchoices = ListField()
-    correctAnswers = ListField()
+    questionID = models.IntegerField()
+    questions = models.TextField()
+    possibleAnswer = models.TextField()
+    
+#     quizname = models.TextField()
+#     question = models.TextField()
+#     answerchoices = ListField()
+#     correctAnswers = ListField()
    
-#TODO: Remove / Merge with QUIZ 
+# TODO: Remove / Merge with QUIZ 
 class QuestionsTable(tables.Table):
-    Questions= tables.Column()
-    Options=tables.Column()
+    Questions = tables.Column()
+    Options = tables.Column()
     Answer = tables.Column()
     
 class pollAnswers(models.Model):
@@ -29,14 +33,15 @@ class pollAnswers(models.Model):
     questionId = models.TextField()
     answer = models.TextField()
 
+# TODO: Remove 
 class Answers(models.Model):
     Name = models.TextField()
     ID = models.IntegerField()
     SIS_ID = models.IntegerField()
     Section = models.TextField()
     Section_Id = models.IntegerField()
-    Section_SIS_Id= models.TextField()
-    Question_Id= models.IntegerField()
-    Question= models.TextField()
-    Submitted= models.TextField()
-    Answer= models.TextField()
+    Section_SIS_Id = models.TextField()
+    Question_Id = models.IntegerField()
+    Question = models.TextField()
+    Submitted = models.TextField()
+    Answer = models.TextField()
