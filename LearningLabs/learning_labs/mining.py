06b34d1ce8bc4b776fc805dfa5e91e65a5b039c7
nimmicv <nimmicv@gmail.com>
2014-11-04 08:38:07 -0800

    Committing Pre-Processing

--- LearningLabs/learning_labs/mining.py
+++ LearningLabs/learning_labs/mining.py
@@ -2,6 +2,9 @@
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 from learning_labs.models import Quiz,Answers
+import nltk
+from nltk.corpus import stopwords
+
 
 def getChartData():
     pi = robjects.r['pi']
@@ -11,26 +14,48 @@ def getChartData():
     docs = base.c("")
     #p=""
     #queryset = StudentAnswers.objects.all();
+    wordDict = {}
     for obj in Answers.objects.all().filter(Question_Id =23156645):
         answer = obj.Answer;
-        print(answer)
-        docs = base.cbind(answer, docs)
+        if answer == None or answer == "":
+            continue
+        print(" Before Preprocess : " + answer)
+        #NLTK pre-process
+        anstokens = nltk.word_tokenize(answer)
+        looper = 0
+        for token in anstokens:
+            anstokens[looper] = token.lower()
+            looper += 1
+        anstokens2 = [token for token in anstokens if (not token in stopwords.words('english'))]
+       # porter = nltk.PorterStemmer()
+       #  looper = 0
+       #  for token in anstokens2:
+       #      anstokens2[looper] = porter.stem(token)
+       #      looper += 1
+        processedAnswer =  ' '.join(anstokens2)
+        #nltk.Text(anstokens2)
+        if processedAnswer in wordDict:
+            wordDict[processedAnswer] +=1
+        else:
+            wordDict[processedAnswer] = 1
+        print("After proess : "+str(processedAnswer))
+
+    return wordDict
+       # docs = base.cbind(answer, docs)
     #p ="first Docuent"
     #p = base.cbind("nimmi", p)
     #docs = base.c("Three musketers", "The three musketers", "musketer", "musketers")
-    ans = tm.VCorpus(tm.VectorSource(docs))
-    ans = tm.tm_map(ans, tm.stripWhitespace)
-    ans = tm.tm_map(ans, tm.content_transformer(base.tolower))
-    ans = tm.tm_map(ans, tm.removeWords, tm.stopwords("english"))
-    dtm= tm.DocumentTermMatrix(ans)
-    words= base.dimnames(dtm)[1]
-    print(" word sis " + str(words))
-    freqVec = r.colSums(r.matrix(dtm,r.length(docs)))
-    ind = 0
-    wordDict = {}
-    for w in words:
-        wordDict[w] = freqVec[ind]
-        ind = ind+ 1
+    # ans = tm.VCorpus(tm.VectorSource(docs))
+    # ans = tm.tm_map(ans, tm.stripWhitespace)
+    # ans = tm.tm_map(ans, tm.content_transformer(base.tolower))
+    # ans = tm.tm_map(ans, tm.removeWords, tm.stopwords("english"))
+    #
+    # dtm= tm.DocumentTermMatrix(ans)
+    # words= base.dimnames(dtm)[1]
+    # print(" word sis " + str(words))
+    # freqVec = r.colSums(r.matrix(dtm,r.length(docs)))
+    # ind = 0
+   # wordDict = {}
+
     #displayDict = {"C#":5, "Python": 6 , "Scala":7, "R":2}
-    print str(wordDict)
-    return wordDict;
+#   print str(wordDict)
--- RminingScript.R
+++ RminingScript.R
@@ -1,13 +1,53 @@
 library(tm)
 library(SnowballC)
-docs <- c("Three musketers", "The three musketers", "musketers", "musketer")
-
+docs <- c(
+  "Alibaba", "The three musketer","musketer","Three alibaba", "Three the alibaba",
+  "musketer", "musketer","three alibaba", "four three alibaba")
+docs<-as.character(livedata2)
+docs<- c("River hawks", "Iver", "Ivar Jacobson", "Ivar jackobson", "UML", "Three Amigos", "Three Amigos", 
+         "three amigos ",  "Ivar ",  "the three amigos ",  "3 Amigoes ",  "Three Amigos ",  "UML ",  
+         "the three aimgos ",  "Three Amigos ",  "Amigos ",  "Three amigos ",  "3 Amigos ",  "three amigos ",  
+         "ivar jacobson ",  "Three Musketeers ",  "the three amigos ",  "3 Amigos ",  "the three amigos ",  
+         "Three Amigos ",  "The Three Amigos ",  "Three Amigos ",  "Ivar ",  "3 Amgoes ",  "Amigos ",  
+         "The Three Amigos ",  "three amigos ",  "Ivar Jacobson    Amigos ",   "three amigos ",  "the three amigos ",
+         "3 amigos ",  "the three amigos ",  "three amigos ",  "the Three Amigos ",  "River Hawks ",  "3 AMIGOS ",  
+         "The Three Amigos ",  "Three Amigos ",  "Have no idea ",  "Booch ",  "Three Amigos ",  "The 3 amigos ", 
+         "Three Amigos ",  "The Three Amigos ",  " Grady Booch, Ivar Jacobson, James Rumbaugh ",  
+         "The Three Amigos ",  "3 amigos ",  "The three Amigos ",  "three amigos ",  "Ivor Jacobson ",  "Ivar ", 
+         "The Three Amigos ",  "three amigos ",  "Ivy ",  "the three Amigos ",  "The Three Amigos ", 
+         "three Amigos ",  "Three Amigos ",   "river hawks ",  "three amigos ",  "Grady booch ",  
+         "The three amigos ",  "amigos ",  "Three Amigos ",  "3 Amigos ",  "The three amigos ",  "No Idea ",  
+         "No idea ",  "River Hawks ",  "The Three Amigos ",  "The Three Amigos ",  "3 amigos ",  "3 amigos ",  
+         "The 3 Amigos ",  "don't know ",  "Three Amigos ",  "don't know ",  "3 amigos ",  "The Three Amigos ", 
+         "jacob ",  "Three Amigos ",  "Jacobson ",  "Three Amigos ",  "THE THREE AMIGOS ",  "three amigos ", 
+         "ivar jacobson ",  "3 musketeers ",  "Grady Booch ",  "Ivar ",  "Don't know ",  "UML ",  "Three Amigos ",  
+         "Three Ambigos ",   "UML ",  "three amigos ",  "3 musketeers ",  "Don't Know ",  "Jacobson ",  "gunja ",
+         "3 amigos ",  "3 musketeers ",  "three amigos ",  "Ivar ",  "three amigos ", 
+         "Three Amigos of software engineering ",  "The three amigos ",  "Ivar Jacobson ",  "3 amigos ",  
+         "river hawks ",  "river hawks ",  "Jacobson ",  "No idea! ",  "James ",  "Three amigos ",  
+         "The Three Amigos ",  "Ivar Jacobson ",  "Anurag ",  "3 Amigos ",  "Three amigos ",  "3 musketeers ", 
+         "Ivar Jacobson ",  "Gang of 4 ",  "three amigos ",  "donald  ",  "three amigos ",  "Ivar Jacobson ", 
+         "Three Amigos ",  "the three amigos ",  "Don't know ",  "Three Amigos ",  "three Amigos ",  "Three Amigos")
+numClusters <-4
+numWords <- 1
 ans <-VCorpus(VectorSource(docs))
- ans<-tm_map(ans, stripWhitespace)
+ans<-tm_map(ans, stripWhitespace)
 ans <- tm_map(ans, content_transformer(tolower))
 ans <- tm_map(ans, removeWords, stopwords("english"))
-#tm_map(ans, stemDocument)
+tm_map(ans, stemDocument)
 dtm <- DocumentTermMatrix(ans)
-freq<-colSums(as.matrix(dtm))
-indFreq<-order(freq)
+##freq<-colSums(as.matrix(dtm))
+##indFreq<-order(freq)
+dtm_tfxidf <- weightTfIdf(dtm)
 
+m <- as.matrix(dtm_tfxidf)
+rownames(m) <- 1:nrow(m)
+norm_eucl <- function(m) m/apply(m, MARGIN=1, FUN=function(x) sum(x^2)^.5)
+m_norm <- norm_eucl(m)
+cl <- kmeans(m_norm,numClusters,50)
+clusters <- 1:numClusters
+#for (i in clusters){
+#cat("Cluster ",i, ":", findFreqTerms(dtm[cl$cluster ==i,],nrow(dtm[cl$cluster==i,])/2)," \n \n")}
+
+for (i in clusters){
+  cat("Cluster ",i, ":", head(names(sort(colSums(as.matrix(dtm[cl$cluster ==i,])),decreasing=TRUE)),numWords)," Count = ", sum(cl$cluster==i) ," \n \n")}
