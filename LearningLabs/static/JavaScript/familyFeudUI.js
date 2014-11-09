4524861c1d4a2cd67a9b644f1eb9e9a2a6923b56
palloabhi <pallavikhandekar212@gmail.com>
2014-11-09 11:22:05 -0800

    1. Have Question and Quiz on top of Mining results 2. Family Feud game loads all Questions same time

--- LearningLabs/static/JavaScript/familyFeudUI.js
+++ LearningLabs/static/JavaScript/familyFeudUI.js
@@ -2,10 +2,18 @@ function AppController($http,$scope) {
 	
 	$http({method: 'GET', url: '/loadFamilyFeudGameData'}).
 	success(function(data, status, headers, config) {
-     	$scope.question = data.question;
-     	$scope.currentQuestion = data.currentQuestion;
-     	$scope.answers = data.gameData;
-     	$scope.quizId = data.quizId;
+		if(data.questions!=undefined){
+			$scope.questions =  data.questions; //Global variables
+			$scope.gameData =  data.gameData;//Global variables
+			$scope.quizId = data.quizId;//Global variables
+			
+			//Load 1st page
+			$scope.currentQuestion = 0; //counter for questions
+     		$scope.question = $scope.questions[$scope.currentQuestion];
+     		$scope.answers = $scope.gameData[$scope.currentQuestion];
+     	}else{
+     		 alert(data);
+     	}
     }).
     error(function(data, status, headers, config) {
       // log error
@@ -13,35 +21,26 @@ function AppController($http,$scope) {
     });
   
 	$scope.loadNextQuestion=function(){
-		$http({method: 'GET', params:{'quizId':$scope.quizId, 'questionId':$scope.currentQuestion, 'isNext':1}, url: '/loadFamilyFeudGameData'}).
-		success(function(data, status, headers, config) {
-			if (data.question != undefined) {
-	     	$scope.question = data.question;
-	     	$scope.currentQuestion = data.currentQuestion;
-	     	$scope.answers = data.gameData;
-	     	$scope.quizId = data.quizId;
-	     	} else {
-	     		 alert(data);
-	     	}
-	    }).
-	    error(function(data, status, headers, config) {
-	      // log error
-	      alert(data);
-	    });
-  
-	
+  		if (($scope.currentQuestion + 1) <  $scope.questions.length){
+  			$scope.currentQuestion = $scope.currentQuestion+1; //counter for questions
+		    $scope.question = $scope.questions[$scope.currentQuestion];
+     		$scope.answers = $scope.gameData[$scope.currentQuestion];
+  		}else{
+  			alert("End of Quiz");
+  		}
+  		
 	};
-	/*
+
 	$scope.loadPreviousQuestion=function(){
 		if($scope.currentQuestion>0){
 			$scope.currentQuestion = $scope.currentQuestion - 1;
-			$scope.question = questions[$scope.currentQuestion]; 
-			$scope.answers = answers[$scope.currentQuestion];
+			$scope.question =  $scope.questions[$scope.currentQuestion]; 
+			$scope.answers = $scope.gameData[$scope.currentQuestion];
 		}
 		else{
 			
 		}
-	};*/
+	};
 }
 
 
